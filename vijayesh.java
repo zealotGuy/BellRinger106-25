ae8ae61011ee9e13a7a85820f0a8af5635cab88d
Aryaman Singh <148281236+zealotGuy@users.noreply.github.com>
2025-10-06 10:52:56 -0700

    Update vijayesh.java

--- vijayesh.java
+++ vijayesh.java
@@ -1,5 +1,6 @@
 public class vijayesh {
     public static void main(String[] args) {
+        //example emails
         String email1 = "vijayeshnandavvvng@gmail.com";
         String email2 = "daily.question123@gmail.com";
         String email3 = "nachiappan.thenappan.nachiappan@gmail.com";
@@ -9,6 +10,7 @@ public class vijayesh {
     }
   
     public static String hivijayesh(String email) {
+        //if theres no @ then its invalid
         int findtheat = email.indexOf('@');
         if (atIndex == -1) {
             return "Invalid email (missing @)";
@@ -16,11 +18,11 @@ public class vijayesh {
       
         String user = email.substring(0, atIndex);
         String[] parts = username.split("\\.");
-      
+      // split w the dot
         if (parts.length < 2) {
             return "email invalid (missing the .)";
         }
-      
+      // no dot= invalid
         String first = parts[0];
         String last = parts[1];
         first = first.toLowerCase();
